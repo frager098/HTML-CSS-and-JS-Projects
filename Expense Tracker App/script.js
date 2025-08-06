e0581163aef31ff933e601bc5938771480ec8e25
frager_098 <hafizmuhammadarham042@gmail.com>
2025-08-06 06:58:40 +0500

    Add delete button logic

--- Expense Tracker App/script.js
+++ Expense Tracker App/script.js
@@ -24,23 +24,23 @@ formElem.addEventListener("submit", handleFormSubmit )
 list.addEventListener('click',handleDelete)
 
 function handleDelete(e){
-    const target = e.target
-    const item = target.closest(".list__item")
+    let item;
+    const target = e.target;
+    if(target.classList.contains("delete")){
+        item = target.closest(".list__item")
+    }
     if (item){
         const id = item.getAttribute("data-id")
         list.removeChild(item);
         const index = transactions.findIndex(transaction => transaction.id == id)
-        alert(index);
     
     if(transactions[index].transactionType == "income"){
-        // updateIncome()
         state.income -= transactions[index].transactionAmount;
         state.balance = state.income - state.expense;
     }
     else{
         state.expense -= transactions[index].transactionAmount;
         state.balance = state.income - state.expense;
-        // updateExpense()
     }
     updateUI()
     transactions.splice(index,1);
@@ -102,12 +102,12 @@ function createListItem(){
             </div>
         </div>
         <div class="svg__container row">  
-            <svg id="delete" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+            <svg class="delete" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                 <title>Delete</title>
                 <!-- !Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc. -->
                 <path d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg>
 
-            <svg title="Edit" id="edit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 
+            <svg title="Edit" class="edit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 
             Fonticons, Inc.-->
             <title>Edit</title>
             <path d="M128.1 64C92.8 64 64.1 92.7 64.1 128L64.1 512C64.1 547.3 92.8 576 128.1 576L274.3 576L285.2 521.5C289.5 499.8 300.2 479.9 315.8 464.3L448 332.1L448 234.6C448 217.6 441.3 201.3 429.3 189.3L322.8 82.7C310.8 70.7 294.5 64 277.6 64L128.1 64zM389.6 240L296.1 240C282.8 240 272.1 229.3 272.1 216L272.1 122.5L389.6 240zM332.3 530.9L320.4 590.5C320.2 591.4 320.1 592.4 320.1 593.4C320.1 601.4 326.6 608 334.7 608C335.7 608 336.6 607.9 337.6 607.7L397.2 595.8C409.6 593.3 421 587.2 429.9 578.3L548.8 459.4L468.8 379.4L349.9 498.3C341 507.2 334.9 518.6 332.4 531zM600.1 407.9C622.2 385.8 622.2 350 600.1 327.9C578 305.8 542.2 305.8 520.1 327.9L491.3 356.7L571.3 436.7L600.1 407.9z"/></svg>
