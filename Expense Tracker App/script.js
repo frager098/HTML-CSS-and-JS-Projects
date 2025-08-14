d28225c9d89e1ae88c19538258e8470c200a5fa8
frager_098 <hafizmuhammadarham042@gmail.com>
2025-08-14 19:02:22 +0500

    Add Responsive design

--- Expense Tracker App/script.js
+++ Expense Tracker App/script.js
@@ -6,7 +6,6 @@ const state = {
     incomeFlag:true
 }
 //UI Selectors
-//const elem =  document.getElementsByTagName("input");
 const formElem = document.querySelector('.form');
 const list = document.querySelector('.list');
 const listItems = document.querySelector('.list').children;
@@ -26,7 +25,6 @@ list.addEventListener('click',handleEdit)
 
 function handleDelete(e){
     const target = e.target;
-    console.log(target)
     if(target.classList.contains("delete")){
         const item = target.closest(".list__item")
         if(!item)return;
@@ -53,7 +51,6 @@ function handleDelete(e){
 
 function handleEdit(e){
     const target = e.target;
-    console.log(target)
     if(target.classList.contains("edit")){
         const item = target.closest(".list__item")
         if(!item)return;
@@ -96,13 +93,10 @@ function undoEditedContent(item,svgs,id){
 function saveEditedContent(item,svgs,index,id){
     const editedTransaction = transactions[index];
     if(editedTransaction.transactionType == "income"){
-        console.log(editedTransaction,"amount")
         updateIncome(-editedTransaction.transactionAmount)
-        console.log(state,"Undoing old--income")
     }
     else{
         updateExpense(-editedTransaction.transactionAmount)
-        console.log(state,"Undoing old--expense")
     }
     transactions[index].transactionName = formElem.name.value;
     transactions[index].transactionAmount = Number(formElem.amount.value);
@@ -125,19 +119,14 @@ function saveEditedContent(item,svgs,index,id){
     if(transactions[index].transactionType == "income"){
         updateIncome(transactions[index].transactionAmount);
         tValue.style.color = "rgb(154, 205, 50)"
-        console.log(state,"Updating new--income")
     }
     else{
         updateExpense(transactions[index].transactionAmount);
         tValue.style.color = "rgb(205, 92, 92)"
-        console.log(state,"Updating new--expense")
 
     }
     updateUI();
     togglePointerEventsOnListItems(id)
-}
-function saveEditedTransaction(e){
-    console.log(formElem.name.value,"value")
 }
 function handleClick(e){
     const classCheckOfSection3Row = section3Row.getAttribute('class').split(" ")[2]
@@ -151,17 +140,13 @@ function handleClick(e){
     }
 } 
 function handleFormSubmit(e){
-    console.log("hello")
     e.preventDefault();
     const formData = new FormData(formElem);
     [name,amount,date] = [formData.get('name'),Number(formData.get('amount')),formData.get('date')]
     formElem.reset()
     const transactionData = createTransaction(name,amount,date)
     localStorage.setItem(`transaction-${transactionData.id}`,JSON.stringify(transactionData));
-    
-    // const li = `<li></li>`;
     createListItem(transactionData)
-    // list.innerHTML += li ;
     if(state.incomeFlag == true ){
         state.incomeFlag = false
         toggleTransactionType();
@@ -204,19 +189,21 @@ function createListItem(transactionData){
 }
 function updateIncome(amount){
     state.income += amount, 
-    state.balance = state.income - state.expense;
+    updateBalance();
 }
 
 function updateExpense(amount){
     state.expense += amount
+    updateBalance();
+}
+function updateBalance(){
     state.balance = state.income - state.expense;
 }
 function updateUI(){
-    console.log(state.incomeFlag,"state")
     showIncome.textContent = "+$"+state.income;  
-    state.expense > 0 ? showExpense.textContent = "-$"+state.expense : showExpense.textContent = "$"+state.expense
+    state.expense!= 0 ? showExpense.textContent = "-$"+state.expense:"$"+state.expense;
     const balance = state.balance;
-    showBalance.textContent = `${(balance > 0 ? "+$": balance < 0 ? "-$":"$")}${Math.abs(balance)}`
+    showBalance.textContent = `${(balance > 0 ? "+$": balance < 0 ? "-$":"$")}${(Math.abs(balance))}`
 }
 
 function toggleTransactionType(){
@@ -228,22 +215,19 @@ function toggleTransactionType(){
 
 //Initialization
 const transactions = []
-// if(localStorage.length != 0 ){
-    
-// }
 let name,amount,date;
 
-let deleteSvg = ` <svg class="svg__delete" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
+const deleteSvg = ` <svg class="svg__delete" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">
                 <title>Delete</title>
                 <!-- !Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc. -->
                 <path class="delete" d="M232.7 69.9L224 96L128 96C110.3 96 96 110.3 96 128C96 145.7 110.3 160 128 160L512 160C529.7 160 544 145.7 544 128C544 110.3 529.7 96 512 96L416 96L407.3 69.9C402.9 56.8 390.7 48 376.9 48L263.1 48C249.3 48 237.1 56.8 232.7 69.9zM512 208L128 208L149.1 531.1C150.7 556.4 171.7 576 197 576L443 576C468.3 576 489.3 556.4 490.9 531.1L512 208z"/></svg>
 `
-let editSvg = `<svg class="svg__edit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 
+const editSvg = `<svg class="svg__edit" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 
             Fonticons, Inc.-->
             <title>Edit</title>
             <path class="edit" d="M128.1 64C92.8 64 64.1 92.7 64.1 128L64.1 512C64.1 547.3 92.8 576 128.1 576L274.3 576L285.2 521.5C289.5 499.8 300.2 479.9 315.8 464.3L448 332.1L448 234.6C448 217.6 441.3 201.3 429.3 189.3L322.8 82.7C310.8 70.7 294.5 64 277.6 64L128.1 64zM389.6 240L296.1 240C282.8 240 272.1 229.3 272.1 216L272.1 122.5L389.6 240zM332.3 530.9L320.4 590.5C320.2 591.4 320.1 592.4 320.1 593.4C320.1 601.4 326.6 608 334.7 608C335.7 608 336.6 607.9 337.6 607.7L397.2 595.8C409.6 593.3 421 587.2 429.9 578.3L548.8 459.4L468.8 379.4L349.9 498.3C341 507.2 334.9 518.6 332.4 531zM600.1 407.9C622.2 385.8 622.2 350 600.1 327.9C578 305.8 542.2 305.8 520.1 327.9L491.3 356.7L571.3 436.7L600.1 407.9z"/></svg>`
 
-let tickSvg = `<svg class="svg__tick" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">'
+const tickSvg = `<svg class="svg__tick" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640">'
 <title>cross</title>
 <path class="tick" d="M530.8 134.1C545.1 144.5 548.3 164.5 537.9 178.8L281.9 530.8C276.4 538.4 267.9 543.1 258.5 543.9C249.1 544.7 240 541.2 233.4 534.6L105.4 406.6C92.9 394.1 92.9 373.8 105.4 361.3C117.9 348.8 138.2 348.8 150.7 361.3L252.2 462.8L486.2 141.1C496.6 126.8 516.6 123.6 530.9 134z"/></svg>`;
 
